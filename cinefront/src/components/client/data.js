77130895ea95d0aa62f68340ac9b0660fdd09f9f
romanobrayde <bcaamal1601@gmail.com>
2023-09-24 20:34:32 -0600

    Prueba01
    Creé archivos en la carpeta Client

---
+++ cinefront/src/components/client/data.js
@@ -0,0 +1,16 @@
+export const data = [
+    {
+        id: 1,
+        img:"https://menuplatino.mx/wp-content/uploads/2020/09/palomitas.png",
+        nameProduct: "Combo 1",
+        price: 100.00,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img:"https://menuplatino.mx/wp-content/uploads/2016/12/combo-palomitasrefresco.png",
+        nameProduct: "Combo 2",
+        price: 110.00,
+        quantity: 1,
+    },
+];
--- cinefront/src/router/index.jsx
+++ cinefront/src/router/index.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ListSalas from "../components/admin/catalogos/sala/ListComponent";
 import ListPeliculasCategoria from "../components/admin/catalogos/peliculascategoria/ListComponent";
 import SubMenuConfig from "../components/admin/catalogos/submenu";
+import Productos from "../components/client/ProductComponent";
 
 // CAMBIAR POR LA RUTA DE INDEX
 const router=createBrowserRouter([
@@ -18,5 +19,9 @@ const router=createBrowserRouter([
         path:"/peliculascategoria",
         element:<ListPeliculasCategoria/>
     },
+    {
+        path:"/productos",
+        element:<Productos/>
+    }
 ])
 export default router;
