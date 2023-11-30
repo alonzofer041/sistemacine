ca13c4fda0d3c48ceba05e89f1f8363e9320be3d
fernandoalonzocamara <63242027+alonzofer041@users.noreply.github.com>
2023-11-29 23:58:38 -0600

    base de datos produccion

--- cineback/db.js
+++ cineback/db.js
@@ -1,9 +1,14 @@
 const {createPool}=require('mysql2/promise');
 const pool=createPool({
-    host:'localhost',
-    user:'root',
+    // host:'localhost',
+    // user:'root',
+    // database:'sistemacine',
+    // port:3306,
+    // password:''
+    host:'servidordemo2.mysql.database.azure.com',
+    user:'alonzofer041',
     database:'sistemacine',
     port:3306,
-    password:''
+    password:'Revan.12.34.5'
 })
 module.exports=pool;
